3ec760df082ed2f75423fb8ff9a0327ffc4bec71
Muhammad Ahmad <ahmadraosanawarali@gmail.com>
2020-06-29 22:17:20 +0500

    This should work fine

--- main.js
+++ main.js
@@ -9,20 +9,9 @@ const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 const authorizeButton = document.getElementById('authorize-button');
 const signoutButton = document.getElementById('signout-button');
 const content = document.getElementById('content');
-const channelForm = document.getElementById('channel-form');
-const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
 const defaultChannel = 'techguyweb';
-
-// Form submit and change channel
-channelForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const channel = channelInput.value;
-
-  getChannel(channel);
-});
 
 // Load auth2 library
 function handleClientLoad() {
